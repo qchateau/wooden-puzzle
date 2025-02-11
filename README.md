c71e7ab321d56ea185434813f6e4e7bcbcd45a8b
Quentin Chateau <quentin.chateau@gmail.com>
2025-02-11 23:06:23 +0100

    Solution

---
+++ README.md
@@ -0,0 +1,30 @@
+# Wooden Puzzle
+
+![Puzzle](pictures/done.jpeg)
+
+A wooden puzzle I got as a gift. Turns out it's pretty difficult.
+
+## Definin the problem space
+
+The problem space is defined as a 3D grid of positions in the range [-2; +2]. Wooden cubes occupy positions -2, 0 or 2 in all directions. Only one dimention of the wooden plates can occupy the -1 or 1 position.
+
+## Definin the pieces
+
+Each of the 8 pieces is defined by the position of its elements, both cubes and plates. Plates are defined as 1, 2 or 3 elements depending on their length.
+I attached pictures of each piece, their reference position and their id.
+
+## Computing pieces permutations
+
+For each piece we find all the position permutations by trying to apply all possible rotation matrices of angles 0, 90, 180 or 270 degrees in each of the 3 axes. We also apply all possible translations of -4, -2, 0, 2 or 4 in each direction. For each rotation/translation, we check if that position is valid by verifying all pieces fit in the problem space.
+
+The piece 0 is special, it is the only one which cannot be translated in any direction. We do not need to rotate it either: it serves as the point of reference.
+
+## Finding the solutions
+
+There are 587 billions possible solutions if we were to try all possible possibles. Instead of checking the validity of each full solution, we can check the validity of a solution using only 2 pieces, then 3 pieces, and so on. If a partial solution does not work, we can skip all possible solutions starting with the invalid partial solution we just checked.
+
+We repeat until finding a full solution. That reduces the number of checks required from 587 billions to 187 thousands.
+
+## Building the puzzle
+
+It turns out there is a single solution, which can be used to place each piece correctly. I am left with a final problem: finding the order in which to build the puzzle. Knowing the exact position of each piece makes this very easy, only a few minutes are needed.
--- main.py
+++ main.py
@@ -1,68 +1,38 @@
-import time
+import argparse
+import logging
 import itertools
 import numpy as np
 from dataclasses import dataclass
 
 
-def valid(pos: np.matrix):
-    return np.all(np.logical_and(-2 <= pos, pos <= 2))
-
-
-def rot_matrix(x: float, y: float, z: float):
-    x_mat = np.matrix(
-        [
-            [1, 0, 0],
-            [0, np.cos(x), np.sin(x)],
-            [0, -np.sin(x), np.cos(x)],
-        ]
-    )
-    y_mat = np.matrix(
-        [
-            [np.cos(y), 0, -np.sin(y)],
-            [0, 1, 0],
-            [np.sin(y), 0, np.cos(y)],
-        ]
-    )
-    z_mat = np.matrix(
-        [
-            [np.cos(z), np.sin(z), 0],
-            [-np.sin(z), np.cos(z), 0],
-            [0, 0, 1],
-        ]
-    )
-    return x_mat * y_mat * z_mat
+log = logging.getLogger()
 
 
 @dataclass
 class Piece:
     id: int
-    cubes: np.matrix
-    plates: np.matrix
+    cubes: np.ndarray
 
     def permutations(self):
         res = []
         for rot in ROTATIONS:
             for tr in TRANSLATIONS:
-                cubes = rot * self.cubes + tr
+                cubes = rot @ self.cubes + tr
                 if valid(cubes):
                     res.append(Piece(id=self.id, cubes=cubes))
         return res
 
-
-ENCODE_MAT = np.matrix([[1, 10, 100]])
-
-
-def solution_is_valid(pieces: list[Piece]):
-    all_cubes = np.concatenate([p.cubes for p in pieces], axis=1)
-    encoded = np.asarray(ENCODE_MAT * all_cubes).reshape(-1).astype(np.int64)
-    unique = np.unique(encoded)
-    return len(unique) == len(encoded)
+    def possible_move_directions(self):
+        return (
+            2
+            - (np.max(self.cubes, axis=1) - np.min(self.cubes, axis=1)).reshape(-1) // 2
+        )
 
 
 PIECES = [
     Piece(
         id=0,
-        cubes=np.matrix(
+        cubes=np.array(
             [
                 [-2, -2, -2],
                 [-2, 0, -2],
@@ -81,7 +51,7 @@ PIECES = [
     ),
     Piece(
         id=1,
-        cubes=np.matrix(
+        cubes=np.array(
             [
                 [-2, -2, -2],
                 [-2, 0, -2],
@@ -100,7 +70,7 @@ PIECES = [
     ),
     Piece(
         id=2,
-        cubes=np.matrix(
+        cubes=np.array(
             [
                 [-2, 0, -2],
                 [0, 0, -2],
@@ -117,7 +87,7 @@ PIECES = [
     ),
     Piece(
         id=3,
-        cubes=np.matrix(
+        cubes=np.array(
             [
                 [-2, 0, -2],
                 [2, 0, -2],
@@ -135,7 +105,7 @@ PIECES = [
     ),
     Piece(
         id=4,
-        cubes=np.matrix(
+        cubes=np.array(
             [
                 [-2, -2, -2],
                 [0, 2, -2],
@@ -151,7 +121,7 @@ PIECES = [
     ),
     Piece(
         id=5,
-        cubes=np.matrix(
+        cubes=np.array(
             [
                 [-2, -2, -2],
                 [0, -2, -2],
@@ -169,7 +139,7 @@ PIECES = [
     ),
     Piece(
         id=6,
-        cubes=np.matrix(
+        cubes=np.array(
             [
                 [-2, -2, -2],
                 [0, -2, -2],
@@ -188,7 +158,7 @@ PIECES = [
     ),
     Piece(
         id=7,
-        cubes=np.matrix(
+        cubes=np.array(
             [
                 [-2, -2, -2],
                 [-2, 0, -2],
@@ -207,34 +177,96 @@ PIECES = [
 ]
 
 
+def valid(pos: np.ndarray):
+    return np.all(np.logical_and(-2 <= pos, pos <= 2))
+
+
+def rot_matrix(x: float, y: float, z: float):
+    x_mat = np.array(
+        [
+            [1, 0, 0],
+            [0, np.cos(x), np.sin(x)],
+            [0, -np.sin(x), np.cos(x)],
+        ]
+    )
+    y_mat = np.array(
+        [
+            [np.cos(y), 0, -np.sin(y)],
+            [0, 1, 0],
+            [np.sin(y), 0, np.cos(y)],
+        ]
+    )
+    z_mat = np.array(
+        [
+            [np.cos(z), np.sin(z), 0],
+            [-np.sin(z), np.cos(z), 0],
+            [0, 0, 1],
+        ]
+    )
+    return np.round(x_mat @ y_mat @ z_mat).astype(np.int64)
+
+
+def solution_is_valid(pieces: list[Piece]):
+    all_cubes = np.concatenate([p.cubes for p in pieces], axis=1).astype(np.int64)
+    encoded = np.asarray(ENCODE_MATRIX @ all_cubes).reshape(-1)
+    unique = np.unique(encoded)
+    valid = len(unique) == len(encoded)
+    return valid
+
+
+def unique(alist: list[np.ndarray] | list[Piece]):
+    unique = []
+    for element in alist:
+        matrix = element.cubes if isinstance(element, Piece) else element
+        if not any(np.all(m == matrix) for m in unique):
+            unique.append(element)
+    return unique
+
+
+ENCODE_MATRIX = np.array([[1, 10, 100]])
+
 TRANLATION_RANGE = [-4, -2, 0, 2, 4]
 TRANSLATIONS = [
-    np.matrix([[x, y, z]]).T
+    np.array([[x, y, z]]).T
     for x in TRANLATION_RANGE
     for y in TRANLATION_RANGE
     for z in TRANLATION_RANGE
 ]
 
 ROTATION_RANGE = [0, np.pi / 2, np.pi, 3 * np.pi / 2]
-ROTATIONS = [
-    rot_matrix(x, y, z)
-    for x in ROTATION_RANGE
-    for y in ROTATION_RANGE
-    for z in ROTATION_RANGE
-]
-
-
-def encode(cube):
-    return np.sum(np.multiply(cube.T, ENCODE_MAT))
+ROTATIONS = unique(
+    [
+        rot_matrix(x, y, z)
+        for x in ROTATION_RANGE
+        for y in ROTATION_RANGE
+        for z in ROTATION_RANGE
+    ]
+)
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+
     pieces_perms = []
     for piece in PIECES:
-        perms = piece.permutations()
-        print(f"Piece {piece.id} has {len(perms)} permutations")
+        perms = unique(piece.permutations())
+        log.info(
+            "Piece %s has %d permutations, move directions: %s",
+            piece.id,
+            len(perms),
+            piece.possible_move_directions(),
+        )
         pieces_perms.append(perms)
     pieces_perms = list(sorted(pieces_perms, key=lambda x: len(x)))
+    for piece_perms in reversed(pieces_perms):
+        if np.all(piece_perms[0].possible_move_directions() == [0, 0, 0]):
+            log.info("Piece %d used as reference point", piece_perms[0].id)
+            piece_perms[:] = piece_perms[:1]
+            break
 
     perm_factors = np.array(
         list(
@@ -245,35 +277,35 @@ def main():
             )
         )
     )
-    print(perm_factors)
     total = np.cumulative_prod(perm_factors)[0]
-    print(f"Total combinations: {total}")
+    log.info("Total combinations: %d", total)
 
+    solutions = []
     perm_factors = perm_factors[1:]
     indices = np.array([0] * len(PIECES))
     depth = len(PIECES) - 1
-
     done = False
-    solutions = []
     count = 0
+    checks = 0
     while not done:
         count += 1
         if count % 1000 == 0:
             progress = (np.dot(indices, perm_factors)) / total
-            print(f"indices: {indices}, {progress*100:.2}%")
+            log.info("Indices: %s, %.2f%% (%s checks)", indices, progress * 100, checks)
 
         skipped_depth = len(PIECES) - 1
         search = [perm[i] for i, perm in zip(indices, pieces_perms)]
         for depth in range(1, len(PIECES)):
             minisearch = search[: (depth + 1)]
 
+            checks += 1
             if not solution_is_valid(minisearch):
                 skipped_depth = depth
                 break
 
             if len(minisearch) == len(search):
                 solutions.append(search)
-                print("Solution found !")
+                log.info("Solution found ! Indices: %s", indices)
                 break
 
         for depth in reversed(range(0, skipped_depth + 1)):
@@ -283,6 +315,16 @@ def main():
             if depth == 0:
                 done = True
             indices[depth:] = 0
+
+    log.info("Covered %d permutations in %d checks", total, checks)
+
+    print("Done, solutions:")
+    for solution in solutions:
+        print("=" * 80)
+        for piece in solution:
+            print()
+            print(f"Piece {piece.id}")
+            print(piece.cubes)
 
 
 if __name__ == "__main__":
